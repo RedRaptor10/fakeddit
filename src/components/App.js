4cfd9e66f610a262ca90ce10d060095380b5a485
RedRaptor10 <weiminh_lam@yahoo.com>
2021-12-07 07:16:00 -0600

    Add auth observer

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { initializeApp } from "firebase/app";
 import { getFirebaseConfig } from "../firebase-config.js";
 import { HashRouter, Routes, Route } from "react-router-dom";
+import { getAuth } from "firebase/auth";
 import Header from "./Header";
-//import Footer from "./Footer";
 import Home from "./Home";
 import Footer from "./Footer";
 import Subreddit from "./Subreddit";
@@ -14,9 +14,23 @@ const App = () => {
 	const firebaseAppConfig = getFirebaseConfig();
 	initializeApp(firebaseAppConfig);
 
+	const [loggedIn, setLoggedIn] = useState(false);
+
+	// Set Logged In observer on componentDidMount
+	useEffect(() => {
+		const auth = getAuth();
+		auth.onAuthStateChanged((user) => {
+			if (user) {
+				setLoggedIn(true);
+			} else {
+				setLoggedIn(false);
+			}
+		});
+	});
+
 	return (
 		<HashRouter>
-			<Header />
+			<Header loggedIn={loggedIn} />
 			<Routes>
 				<Route exact path="/" element={<Home />} />
 				<Route exact path="/r/:slug" element={<Subreddit />} />
