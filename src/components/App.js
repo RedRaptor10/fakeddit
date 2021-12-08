1bcca60a4ccbd9e6326861c885be62fd4185c2e1
RedRaptor10 <weiminh_lam@yahoo.com>
2021-12-08 08:25:45 -0600

    Add user data

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getFirebaseConfig } from "../firebase-config.js";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import { getAuth } from "firebase/auth";
+import { getFirestore, collection, query, where, getDocs } from "firebase/firestore";
 import Header from "./Header";
 import Home from "./Home";
 import Footer from "./Footer";
@@ -15,26 +16,44 @@ const App = () => {
 	initializeApp(firebaseAppConfig);
 
 	const [loggedIn, setLoggedIn] = useState(false);
+	const [user, setUser] = useState();
 
 	// Set Logged In observer on componentDidMount
 	useEffect(() => {
 		const auth = getAuth();
-		auth.onAuthStateChanged((user) => {
-			if (user) {
+		auth.onAuthStateChanged((userAuth) => {
+			if (userAuth) {
+				const db = getFirestore();
+
+				// Get user data
+				const getUser = async () => {
+					const q = query(collection(db, "users"), where("email", "==", auth.currentUser.email));
+					const querySnapshot = await getDocs(q);
+					querySnapshot.forEach((doc) => {
+						setUser({
+							email: doc.data().email,
+							username: doc.data().username,
+							password: doc.data().password
+						});
+					});
+				};
+
+				getUser();
 				setLoggedIn(true);
 			} else {
 				setLoggedIn(false);
+				setUser(null);
 			}
 		});
-	});
+	}, []);
 
 	return (
 		<HashRouter>
 			<Header loggedIn={loggedIn} />
 			<Routes>
 				<Route exact path="/" element={<Home />} />
-				<Route exact path="/r/:slug" element={<Subreddit />} />
-				<Route exact path="/r/:slug/submit" element={<Submit />} />
+				<Route exact path="/r/:slug" element={<Subreddit loggedIn={loggedIn} />} />
+				<Route exact path="/r/:slug/submit" element={<Submit loggedIn={loggedIn} user={user} />} />
 			</Routes>
 			<Footer />
 		</HashRouter>
